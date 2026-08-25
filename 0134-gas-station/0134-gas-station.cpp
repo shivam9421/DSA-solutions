1eda1b6e7d4d65fc2993bc8b45c2118046e7e86b
shivam kumar singh <shivamsingh878998@gmail.com>
2026-08-26 01:15:30 +0530

    Time: 4 ms (23.41%), Space: 112.4 MB (61.76%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int sumcost=0;
-        int sumgas=0;
-        for(int i=0;i<gas.size();i++){
-            sumcost+=cost[i];
-            sumgas+=gas[i];
-        }
-        if(sumgas<sumcost)return -1;
+
+        int istravel_current=0;
         int istravel=0;
         int startindex=0;
         for(int i=0;i<cost.size();i++){
-            istravel+=gas[i]-cost[i];
-            if(istravel<0){
+            int gain=gas[i]-cost[i];
+            istravel+=gain;
+            istravel_current+=gain;
+            if(istravel_current<0){
                 startindex=i+1;
-                istravel=0;
+                istravel_current=0;
             }
         }
+        if(istravel<0)return -1;
         return startindex;
         
     }
